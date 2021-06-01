49d61129016d8ce33962e0e64895ac08c4919806
Faramita <39052095+yyinfaramita@users.noreply.github.com>
2021-06-01 09:45:10 +0800

    Update dataloaders.py

--- crystal/dataloaders.py
+++ crystal/dataloaders.py
@@ -11,78 +11,57 @@ class custum_CIFAR10(CIFAR10):
     def __init__(self, target, num, config, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.config = config
+        data_sizes = config.general.train_target_size
 
-        self.data, self.targets = shuffle(self.data, self.targets, random_state=config.general.seed)
-
-        if self.train:
-            if target:
-                self.data = self.data[:config.general.train_target_size]
-                self.targets = self.targets[:config.general.train_target_size]
+        # self.data, self.targets = shuffle(self.data, self.targets, random_state=config.general.seed)
+        if target:
+            if self.train:
+                self.data = self.data[:data_sizes]
+                self.targets = self.targets[:data_sizes]
             else:
+                index1 = (data_sizes * (num))
+                index2 = (data_sizes * (num + 1))
 
-                index1 = (config.general.train_target_size * (num + 1)) % len(self.data)
-                index2 = (config.general.train_target_size * (
-                        num + 2)) % len(self.data)
-                if index2 < index1:
-                    self.data = np.append(self.data[index1:], self.data[
-                                                              config.general.train_target_size: config.general.train_target_size + index2],
-                                          axis=0)
-                    self.targets = np.append(self.targets[index1:], self.targets[
-                                                                    config.general.train_target_size: config.general.train_target_size + index2],
-                                             axis=0)
-                    self.targets = self.targets.astype(int)
-                else:
-                    self.data = self.data[index1:index2]
-                    self.targets = self.targets[index1:index2]
+                self.data = self.data[index1:index2]
+                self.targets = self.targets[index1:index2]
+
+            datasets = []
+            for img, target in zip(self.data, self.targets):
+                img = Image.fromarray(img)
+                img = self.transform(img)
+                # target = self.target_transform(t)
+
+                datasets.append((img, target))
+
+            self.datasets = datasets
         else:
-            if target:
-                self.data = self.data[:config.general.test_target_size]
-                self.targets = self.targets[:config.general.test_target_size]
+            self.data = self.data[data_sizes:]
+            self.targets = self.targets[data_sizes:]
 
-            else:
+            tmp = random.sample(range(0, len(self.data)), data_sizes * 2)
+            train_tmp = tmp[:data_sizes]
+            test_tmp = tmp[data_sizes:]
 
-                index1 = (config.general.test_target_size * (num + 1)) % len(self.data)
-                index2 = (config.general.test_target_size * (
-                        num + 2)) % len(self.data)
-                # print(index1, index2)
-                if index2 < index1:
-                    self.data = np.append(self.data[index1:], self.data[
-                                                              config.general.test_target_size: config.general.test_target_size + index2],
-                                          axis=0)
-                    self.targets = np.append(self.targets[index1:], self.targets[
-                                                                    config.general.test_target_size: config.general.test_target_size + index2],
-                                             axis=0)
-                    self.targets = self.targets.astype(int)
-                else:
-                    self.data = self.data[index1:index2]
-                    self.targets = self.targets[index1:index2]
+            train_data = [self.data[i] for i in train_tmp]
+            test_data = [self.data[i] for i in test_tmp]
+            train_targets = [self.targets[i] for i in train_tmp]
+            test_targets = [self.targets[i] for i in test_tmp]
 
-        # self.data.dtype = int
+            trains, tests = [], []
+            for d, target in zip(train_data, train_targets):
+                img = Image.fromarray(d)
+                img = self.transform(img)
+                # target = self.target_transform(t)
 
+                trains.append((img, target))
 
-    def __getitem__(self, index):
-        """
-        Args:
-            index (int): Index
+            for d, target in zip(test_data, test_targets):
+                img = Image.fromarray(d)
+                img = self.transform(img)
+                # target = self.target_transform(t)
 
-        Returns:
-            tuple: (image, target) where target is index of the target class.
-        """
-        if self.train:
-            index = index % self.config.general.train_target_size
-        else:
-            index = index % self.config.general.test_target_size
-        img, target = self.data[index], self.targets[index]
+                tests.append((img, target))
 
-        # doing this so that it is consistent with all other datasets
-        # to return a PIL Image
-        img = Image.fromarray(img)
-
-        if self.transform is not None:
-            img = self.transform(img)
-
-        if self.target_transform is not None:
-            target = self.target_transform(target)
-
-        return img, target
+            self.trains = trains
+            self.tests = tests
 
