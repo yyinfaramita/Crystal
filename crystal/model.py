731d65c7746372e848de04194c874b72206dcd23
Faramita <39052095+yyinfaramita@users.noreply.github.com>
2021-06-01 09:42:49 +0800

    Update model.py

--- crystal/model.py
+++ crystal/model.py
@@ -30,43 +30,6 @@ class Net_cifar10(nn.Module):
         self.output = torch.cat((x1.view(-1, 16 * 16 * 16), x2, x3, x4), 1)
         return x4
 
-class White_attack(nn.Module):
-
-    def __init__(self, len1, len2):
-        super(White_attack, self).__init__()
-
-        self.output = nn.Sequential(
-            nn.Linear(len1, 128),
-            nn.ReLU(),
-            nn.Linear(128, 64),
-            nn.ReLU()
-        )
-        self.loss = nn.Sequential(
-            nn.Linear(1, 128),
-            nn.ReLU(),
-            nn.Linear(128, 64),
-            nn.ReLU()
-        )
-        self.label = nn.Sequential(
-            nn.Linear(len2, 128),
-            nn.ReLU(),
-            nn.Linear(128, 64),
-            nn.ReLU()
-        )
-        self.encodes = nn.Sequential(
-            nn.Linear(64 * 3, 256),
-            nn.ReLU(),
-            nn.Linear(256, 128),
-            nn.ReLU(),
-            nn.Linear(128, 64),
-            nn.ReLU(),
-            nn.Linear(64, 1)
-        )
-
-    def forward(self, x):
-        x1 = self.output(x[0])
-        x2 = self.loss(x[1])
-        x3 = self.label(x[2])
 
         y = torch.cat((x1, x2, x3), 1)
         z = self.encodes(y)
