311e17184c799bf8683974bfd841c39e97c42376
Faramita <39052095+yyinfaramita@users.noreply.github.com>
2021-06-07 11:09:11 +0800

    Update model.py

--- crystal/model.py
+++ crystal/model.py
@@ -13,11 +13,11 @@ class Net_cifar10(nn.Module):
     def __init__(self):
         super().__init__()
         self.conv1 = nn.Conv2d(3, 16, 3, padding=1)
-        self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
         self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
         self.fc1 = nn.Linear(32 * 8 * 8, 512)
-        self.fc2 = nn.Linear(512, 10)
-
+        self.fc2 = nn.Linear(512, 128)
+        self.fc3 = nn.Linear(128, 10)
         self.output = None
 
     def forward(self, x):
@@ -25,7 +25,7 @@ class Net_cifar10(nn.Module):
         x2 = self.pool(F.relu(self.conv2(x1)))
         x2 = x2.view(-1, 32 * 8 * 8)
         x3 = F.relu(self.fc1(x2))
-        x4 = self.fc2(x3)
+        x4 = F.relu(self.fc2(x3))
+        x5 = self.fc3(x4)
 
-        self.output = torch.cat((x1.view(-1, 16 * 16 * 16), x2, x3, x4), 1)
-        return x4
+        return x5
