4611fbc1f2e14965918aaa767f8313a2f1c0faf7
Faramita <39052095+yyinfaramita@users.noreply.github.com>
2021-06-29 09:37:23 +0800

    Update model.py

--- crystal/model.py
+++ crystal/model.py
@@ -29,3 +29,25 @@ class Net_cifar10(nn.Module):
         x5 = self.fc(x4)
 
         return x5
+
+class Net_cifar100(nn.Module):
+    def __init__(self, n_classes):
+        super(LeNet_cifar100, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 64)
+        self.fc2 = nn.Linear(64, 512)
+        self.fc = nn.Linear(512, n_classes)
+        # self.softmax = nn.Softmax(dim=1)
+        self.output = None
+
+    def forward(self, x):
+        x1 = self.pool(F.relu(self.conv1(x)))
+        x2 = self.pool(F.relu(self.conv2(x1)))
+        x2 = x2.view(-1, 16 * 5 * 5)
+        x3 = F.relu(self.fc1(x2))
+        x4 = F.relu(self.fc2(x3))
+        x5 = self.fc(x4)
+
+        return x5
