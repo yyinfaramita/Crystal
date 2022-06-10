f910a8ccd58a4a0ce671219f059913959c0c00cb
Faramita <39052095+yyinfaramita@users.noreply.github.com>
2022-06-10 17:23:54 +0800

    Update runs.py

--- crystal/runs.py
+++ crystal/runs.py
@@ -8,11 +8,11 @@ if __name__ == '__main__':
     config = config()
 
     # parser = argparse.ArgumentParser(description='Process some integers.')
-    # parser.add_argument('type', type=str, default="DB_DC_GA")
+    # parser.add_argument('type', type=str, default="DB+DC+GA")
     # parser.add_argument('cuda', type=int, default=0)
     # parser.add_argument('dataset', type=str, default="CIFAR10")
-    # parser.add_argument('white', type=int, default=0)
-    # parser.add_argument('fed', type=int, default=1)
+    # parser.add_argument('white', type=int, default=0) # if white-box
+    # parser.add_argument('fed', type=int, default=1) # if crystal-box
     # args = parser.parse_args()
 
     # types = args.type
@@ -26,8 +26,8 @@ if __name__ == '__main__':
 
     print("CIFAR10")
     config.set_subkey("statistics", "dataset", "CIFAR10")
-    config.set_subkey("learning", "epochs", 100)
-    config.set_subkey("learning", "learning_rate", 0.0025)
+    config.set_subkey("learning", "epochs", 40)
+    config.set_subkey("learning", "learning_rate", 0.001)
     config.set_subkey("general", "number_shadow_model", 10)
     config.set_subkey("general", "test_target_size", 5000)
     config.set_subkey("general", "train_target_size", 5000)
